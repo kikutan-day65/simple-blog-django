e9757228328243d63be4eb186bdeff22e8f15f4d
Haruto Mori <baystarsman@outlook.jp>
2023-03-03 16:46:24 +0100

    display all posts in the home

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Post    # add database to the admin panel
 
 # Register your models here.
+admin.site.register(Post)   # add database to the admin panel
--- posts/models.py
+++ posts/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from datetime import datetime
 
 # Create your models here.
-class Posts(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=100)
     body = models.CharField(max_length=10000000)
-    ceated_at = models.DateTimeField(default=datetime.now, blank=True)
-    
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
